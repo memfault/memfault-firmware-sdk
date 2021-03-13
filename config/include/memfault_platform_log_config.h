706bcdc24bbfc9090db0b70e541e7a847cf2c34d
Sean Hudson <seanh@happy.ai>
2021-03-12 23:58:20 -0600

    log: replace failing hpy prints with default impl
    Signed-off-by: Sean Hudson <seanh@happy.ai>

--- config/include/memfault_platform_log_config.h
+++ config/include/memfault_platform_log_config.h
@@ -1,7 +1,3 @@
 //! @file memfault_platform_log_config.h
 #pragma once
 
-#define MEMFAULT_LOG_ERROR(...) hpy_printf (false, HPY_MSG_LEVEL_ERROR, __VA_ARGS__)
-#define MEMFAULT_LOG_WARN(...) hpy_printf (false, HPY_MSG_LEVEL_WARN, __VA_ARGS__)
-#define MEMFAULT_LOG_INFO(...) hpy_printf (false, HPY_MSG_LEVEL_INFO, __VA_ARGS__)
-#define MEMFAULT_LOG_DEBUG(...) hpy_printf (false, HPY_MSG_LEVEL_DEBUG, __VA_ARGS__)
