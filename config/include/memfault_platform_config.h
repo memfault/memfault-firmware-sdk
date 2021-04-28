6031ff0b7732b8b38337c1ebe7816eaed16cac04
Sean Hudson <seanh@happy.ai>
2021-04-28 02:28:58 -0500

    config: drop option in favor of default
    Signed-off-by: Sean Hudson <seanh@happy.ai>

--- config/include/memfault_platform_config.h
+++ config/include/memfault_platform_config.h
@@ -1,5 +1,4 @@
 #define MEMFAULT_USE_GNU_BUILD_ID 1
-#define MEMFAULT_TRACE_REASON_USER_DEFS_FILE "memfault_trace_reason_user_config.def"
 
 #define MEMFAULT_PLATFORM_COREDUMP_NOINIT_SECTION_NAME "retention_mem_uninit"
 
