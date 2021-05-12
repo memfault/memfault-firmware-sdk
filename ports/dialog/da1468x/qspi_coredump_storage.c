fbc06ce37f0e627fa2743d4ab04db6dd17e2ef68
Sean Hudson <seanh@happy.ai>
2021-05-11 21:26:31 -0500

    coredump: drop possible duplicate call to ad_nvms_init()
    Signed-off-by: Sean Hudson <seanh@happy.ai>

--- ports/dialog/da1468x/qspi_coredump_storage.c
+++ ports/dialog/da1468x/qspi_coredump_storage.c
@@ -120,7 +120,8 @@ static void prv_coredump_writer_assert_and_reboot(int error_code) {
 // This is Dialog specific and not extern'd in a header file.
 // NOTE: The user must call ad_nvms_init() before calling this function.
 void memfault_platform_coredump_storage_boot(void) {
-  ad_nvms_init();
+  // NOTE: The Dialog SDK automatically calls the NVMS system initialization.
+  //ad_nvms_init();
   nvms_t part = ad_nvms_open(MEMFAULT_PLATFORM_COREDUMP_STORAGE_PARTITION);
   if (part == NULL) {
     MEMFAULT_LOG_ERROR("Could not locate partition for coredump storage, has ad_nvms_init() been called?");
